9745ed164bb567280628c7370f56674e85a832f0
ritzDev07 <ritzocite@gmail.com>
2024-01-19 19:51:54 +0800

    Added CORS middleware for frontend with credentials

--- api/src/index.ts
+++ api/src/index.ts
@@ -20,7 +20,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        credentials: true,
+    })
+);
 
 //Handling routes
 app.use("/api/auth", authRoutes) // For login user
--- client/src/api-client.ts
+++ client/src/api-client.ts
@@ -5,8 +5,10 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const register = async (formData: RegisterFromData) => {
     const response = await fetch(`${API_BASE_URL}/api/users/register`, {
         method: 'POST',
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
+
         },
         body: JSON.stringify(formData),
     });

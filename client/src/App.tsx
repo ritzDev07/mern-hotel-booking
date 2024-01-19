d5f8748b4af7eff1b3d647452986972b66b44315
ritzDev07 <ritzocite@gmail.com>
2024-01-19 12:19:11 +0800

    Added User Registration Form Layout & Installed react-hook-form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,31 @@
 import { BrowserRouter as Router, Route, Routes, Navigate, } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App = () => {
 
 	return (
 		<Router>
 			<Routes>
-				<Route path="/" element={<Layout>
-					<p>HomePage</p>
-				</Layout>} />
-				<Route path="/sign-in" element={<Layout>
-					<p>SignIn page</p>
-				</Layout>} />
+
+				<Route path="/" element={
+					<Layout>
+						<p>HomePage</p>
+					</Layout>
+				} />
+
+				<Route path="/sign-in" element={
+					<Layout>
+						<p>SignIn page</p>
+					</Layout>
+				} />
+
+				<Route path="/register" element={
+					<Layout>
+						<Register />
+					</Layout>
+				} />
+
 			</Routes>
 		</Router>
 	)
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,129 @@
+import { useForm } from "react-hook-form";
+
+type RegisterFromData = {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
+
+const Register = () => {
+
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: { errors }, } = useForm<RegisterFromData>();
+
+    const onSubmit = handleSubmit((data) => {
+        console.log(data)
+    })
+
+    return (
+        <form className="flex flex-col gap-5" onSubmit={onSubmit} >
+            <h2 className="text-3xl font-bold">Create an Account</h2>
+
+            <div className="flex flex-col md:flex-row gap-5">
+                <label className="text-gray-700 text-sm font-bold flex-1"
+                    {...register("firstName", { required: "This field is required" })}>
+                    First Name
+
+                    <input
+                        name="firstName"
+                        className="border rounded w-full py-1 px-2 font-normal">
+                    </input>
+
+                    {errors.firstName && (
+                        <span className="text-red-500">{errors.firstName.message}</span>
+                    )}
+
+                </label>
+
+                <label className="text-gray-700 text-sm font-bold flex-1"
+                    {...register("lastName", { required: "This field is required" })}>
+                    Last Name
+
+                    <input
+                        name="lastName"
+                        className="border rounded w-full py-1 px-2 font-normal">
+                    </input>
+
+                    {errors.lastName && (
+                        <span className="text-red-500">{errors.lastName.message}</span>
+                    )}
+
+                </label>
+            </div>
+
+            <label className="text-gray-700 text-sm font-bold flex-1"
+                {...register("email", { required: "This field is required" })}>
+                Email
+
+                <input
+                    type="email"
+                    name="email"
+                    className="border rounded w-full py-1 px-2 font-normal">
+                </input>
+
+                {errors.email && (
+                    <span className="text-red-500">{errors.email.message}</span>
+                )}
+
+            </label>
+
+            <label className="text-gray-700 text-sm font-bold flex-1"
+                {...register("password", { required: "This field is required", minLength: { value: 8, message: "Password must be at least 8 characters" } })}>
+                Password
+
+                <input
+                    type="password"
+                    name="password"
+                    className="border rounded w-full py-1 px-2 font-normal">
+                </input>
+
+                {errors.password && (
+                    <span className="text-red-500">{errors.password.message}</span>
+                )}
+
+            </label>
+
+            <label className="text-gray-700 text-sm font-bold flex-1"
+                {...register("confirmPassword", {
+                    validate: (val) => {
+                        if (!val) {
+                            return "this field is required"
+                        }
+                        else if (watch("password") !== val) {
+                            return " Passwords do not match."
+                        }
+
+                    }
+                })}>
+                Confrim Password
+
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    className="border rounded w-full py-1 px-2 font-normal">
+                </input>
+
+                {errors.confirmPassword && (
+                    <span className="text-red-500">{errors.confirmPassword.message}</span>
+                )}
+
+            </label>
+
+            <span>
+                <button
+                    type="submit"
+                    className="bg-green-600 text-white p-2 font-bold hover:bg-green-500 text-xl">
+                    Create Account
+                </button>
+            </span>
+
+        </form>
+    )
+}
+
+export default Register;

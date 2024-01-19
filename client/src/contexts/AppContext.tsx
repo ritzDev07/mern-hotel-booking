fbab03420515cf3619a1e597de531fabb7efc615
ritzDev07 <ritzocite@gmail.com>
2024-01-19 15:32:37 +0800

    Added Toast notification & User redirection to homepage after successful registration

--- client/src/contexts/AppContext.tsx
+++ client/src/contexts/AppContext.tsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Toast from "../components/Toast";
 
 // Define the structure of a toast message
 type ToastMessage = {
@@ -17,13 +18,23 @@ const AppContext = React.createContext<AppContext | undefined>(undefined);
 // Create a provider component for the AppContext
 export const AppContextProvider = ({ children }: { children: React.ReactNode }) => {
 
+    const [toast, setToast] = useState<ToastMessage | undefined>(undefined)
+
     // Provide a default value for the context with showToast function
     return (
         <AppContext.Provider value={{
             showToast: (toastMessage) => {
                 console.log(toastMessage)
+                setToast(toastMessage);
             }
         }}>
+            {toast && (
+                <Toast
+                    message={toast.messages}
+                    type={toast.type}
+                    onClose={() => setToast(undefined)}
+                />
+            )}
             {children}
         </AppContext.Provider>
     )
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFromData = {
     firstName: string;
@@ -13,6 +14,7 @@ export type RegisterFromData = {
 
 const Register = () => {
 
+    const navigate = useNavigate();
     const { showToast } = useAppContext();
 
     const {
@@ -29,6 +31,7 @@ const Register = () => {
             showToast({ messages: "Registration succesful!", type: "SUCCESS" })
             // Reset the form after successful submission
             reset();
+            navigate("/");
         },
         onError: (error: Error) => {
             console.log(error.message);

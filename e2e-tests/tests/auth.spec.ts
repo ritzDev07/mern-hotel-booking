f0ce8034e3cc389b3c90d197cc142abf659e1bf0
ritzDev07 <ritzocite@gmail.com>
2024-01-23 12:15:26 +0800

    TEST: User Registration

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 const UI_URL = "http://localhost:5173/";
 
-test('should allow the user to sign in', async ({ page }) => {
+test('Should allow the user to sign in', async ({ page }) => {
 	await page.goto(UI_URL);
 
 	//get the sign in button
@@ -20,4 +20,26 @@ test('should allow the user to sign in', async ({ page }) => {
 	await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
 	await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 
+});
+
+test("Should allow user to register", async ({ page }) => {
+	await page.goto(UI_URL);
+
+	await page.getByRole("link", { name: "Sign in" }).click();
+	await page.getByRole("link", { name: "Create an Account" }).click();
+
+	await expect(page.getByRole("heading", { name: "Create an Account" })).toBeVisible();
+
+	await page.locator("input[name=firstName]").fill("test firstname");
+	await page.locator("input[name=lastName]").fill("test lastname");
+	await page.locator("input[name=email]").fill("test1@email.com");
+	await page.locator("input[name=password]").fill("test1234");
+	await page.locator("input[name=confirmPassword]").fill("test1234");
+
+	await page.getByRole("button", { name: "Create Account" }).click();
+	
+	await expect(page.getByText("Registration succesful!")).toBeVisible();
+	await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+	await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+	await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
 });

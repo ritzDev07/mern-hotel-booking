33339f00bb54764fbfab0df228ee28bff3504be2
ritzDev07 <ritzocite@gmail.com>
2024-01-25 13:29:15 +0800

    Completed My Hotel page layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import { useAppContext } from "./contexts/AppContext";
 import AddHotel from "./pages/AddHotel";
+import MyHotels from "./pages/MyHotels";
 
 const App = () => {
 	const { isLoggedIn } = useAppContext();
@@ -36,6 +37,13 @@ const App = () => {
 							path="/add-hotel" element={
 								<Layout>
 									<AddHotel />
+								</Layout>
+							}
+						/>
+						<Route
+							path="/my-hotels" element={
+								<Layout>
+									<MyHotels />
 								</Layout>
 							}
 						/>

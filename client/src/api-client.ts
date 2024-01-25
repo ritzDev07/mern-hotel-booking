dd6d94e637d1b52884935593df568d1a2328811f
ritzDev07 <ritzocite@gmail.com>
2024-01-25 11:10:57 +0800

    Feat: Add client-side function to fetch user's hotel & server-side route to handle the request

--- client/src/api-client.ts
+++ client/src/api-client.ts
@@ -1,5 +1,6 @@
 import { RegisterFromData } from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
+import { HotelType } from "../../api/src/models/hotel";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -79,6 +80,18 @@ export const addMyHotel = async (hotelFormData: FormData) => {
 
     if (!response.ok) {
         throw new Error("Failed to add Hotel");
+    }
+
+    return response.json();
+};
+
+export const fetchMyHotels = async (): Promise<HotelType[]> => {
+    const response = await fetch(`${API_BASE_URL}/api/my-hotels`, {
+        credentials: "include",
+    });
+
+    if (!response.ok) {
+        throw new Error("Error fetching hotels");
     }
 
     return response.json();

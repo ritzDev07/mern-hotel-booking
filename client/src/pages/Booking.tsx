0926ab14f920f17ba1d2b2ae806c832d5c3e924e
ritzDev07 <ritzocite@gmail.com>
2024-01-28 10:28:21 +0800

    Added Booking section and Summary details

--- client/src/pages/Booking.tsx
+++ client/src/pages/Booking.tsx
@@ -1,19 +1,56 @@
 import { useQuery } from "react-query";
 import * as apiClient from "../api-client";
 import BookingForm from "../forms/BookingForm/BookingForm";
+import { useSearchContext } from "../contexts/SearchContext";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BookingDetailsSummary from "../components/BookingDetailsSummary";
 
-
+// Defining Booking functional component
 const Booking = () => {
+
+    // Retrieving search context and hotelId from route parameters
+    const search = useSearchContext();
+    const { hotelId } = useParams();
+
+    const [numberOfNights, setNumberOfNights] = useState<number>(0);
+
+    // Effect to calculate the number of nights when check-in or check-out dates change
+    useEffect(() => {
+        if (search.checkIn && search.checkOut) {
+            const nights = Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+                (1000 * 60 * 60 * 24);
+
+            setNumberOfNights(Math.ceil(nights));
+        }
+    }, [search.checkIn, search.checkOut]);
+
+    const { data: hotel } = useQuery("fetchHotelByID", () =>
+        apiClient.fetchHotelById(hotelId as string), {
+        enabled: !!hotelId,
+    }
+    );
+
     const { data: currentUser } = useQuery(
         "fetchCurrentUser",
         apiClient.fetchCurrentUser
     );
 
+    // If hotel data is not available, return an empty fragment
+    if (!hotel) {
+        return <></>;
+    }
+
     return (
         <div className="grid md:grid-cols-[1fr_2fr]">
-            <div className="bg-green-300">
-                booking summary
-            </div>
+            <BookingDetailsSummary
+                checkIn={search.checkIn}
+                checkOut={search.checkOut}
+                adultCount={search.adultCount}
+                childCount={search.childCount}
+                numberOfNights={numberOfNights}
+                hotel={hotel}
+            />
             {currentUser && <BookingForm
                 currentUser={currentUser}
             />}
